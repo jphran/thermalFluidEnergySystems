6f1f487cb82b7bf002e2498c69b77ab3eb995e3f
jphran <41977074+jphran@users.noreply.github.com>
2020-04-03 12:42:10 -0600

    moving workstations

--- Lab9_FPConvection/Lab9_FPConvection.m
+++ Lab9_FPConvection/Lab9_FPConvection.m
@@ -3,7 +3,7 @@
 % Dr. Metzger, U of U
 % Lab 9, Flat Plate Convection
 
-clc; clear; close all;
+clc; clear;
 
 data = load('ConvectionData.dat');
 T_s = data(:, 2) + 273.15; %[K]
@@ -38,7 +38,7 @@ V_fs = sqrt(2*P_dyn/rho); %free stream velocity[m/s]
 netHeatFlux_top = V_AC^2/(2*R*L_h*w); %perfect efficiency[W/m^2]
 %b, heat trans coeff
 localHeatTransCoeff = netHeatFlux_top./(T_s-T_inf); %[W/(m^2*K)]
-avgHeatTransCoeff = 1/(loc(length(loc))-loc(1)) * trapz(localHeatTransCoeff, loc);
+avgHeatTransCoeff = 1/(loc(length(loc))-loc(1)) * trapz(loc, localHeatTransCoeff);
 %c, nusselt num
 T_f = (T_s + T_inf)/2; %film temp[K]
 [rho_f, mu_f, k_f, Cp_f] = AirProperties(T_f, P_amb*1e3);
@@ -65,12 +65,37 @@ for i = 1:length(Re_x)
     end
 end
 
+theoreticalLocalHeatTransCoeff = (k_bar./loc).*theoreticalLocalNusselt;
 
+%b, avg nusselt and avg heat trans coeff
+Re_L = (V_fs*L)/(mu_bar/rho_bar);
 
+%since Re_L < Re_cr, use laminar eqn
+theoAvgHeatTransCoeff = 2*(k_bar/(L-zeta))*(0.453*Re_L^(1/2)*Pr^(1/3))*(1-(zeta/L)^(3/4))^(2/3);
+%theoAvgHeatTransCoeff_turb = (5/4)*(k_bar/(L-zeta))*(0.031*Re_L^(4/5)*Pr^(3/5))*(1-(zeta/L)^(9/10))^(8/9);
 
+theoAvgNusselt = (theoAvgHeatTransCoeff*L)/k_bar;
 
+%c, predicted surf temp dist
+theoLocalSurfTemp = T_inf + netHeatFlux_top./theoreticalLocalHeatTransCoeff;
 
+%d, predicted heat trans due to convection
+theoNetHeatFlux_top = theoreticalLocalHeatTransCoeff.*(T_s-T_inf);
+theoHeatTransConv = (w*L_h/(loc(length(loc))-loc(1)))*trapz(loc, theoNetHeatFlux_top);
 
+%% 3, est heat trans due to radiation
+sigma = 5.6703e-8; %stephann-boltzman const[W/(m^2*K^4)]
+epsilon = 0.7; %approx plate emissivity
 
+heatFluxRad = epsilon*sigma.*(T_s.^4 - T_inf^4);
+heatFluxRad_L = 1/(loc(length(loc))-loc(1))*trapz(loc,heatFluxRad);
 
+heatTransRad = L_h*w*heatFluxRad_L;
 
+%% Plots
+x_prime = (loc-zeta)./L_h; %nondimentional location
+
+%% 1a
+close all;
+figure();
+plot(x_prime, T_s);
