6cc104334044e0a94f3c99044c4eb88fb21816ac
jphran <41977074+jphran@users.noreply.github.com>
2020-04-03 17:07:52 -0600

    finished flat plate convection calculations

--- Lab9_FPConvection/Lab9_FPConvection.m
+++ Lab9_FPConvection/Lab9_FPConvection.m
@@ -2,6 +2,7 @@
 % MEEN 4650, TFES
 % Dr. Metzger, U of U
 % Lab 9, Flat Plate Convection
+% TODO: Local nusselt, theo surf temp due to conv and rad
 
 clc; clear;
 
@@ -23,15 +24,18 @@ L_sh = 76.2 * 1e-3; %[m]
 w_sh = 25.4 * 1e-3; %[m]
 R = 156.7; %total resistance of heaters in parrallel[ohm]
 V_AC = 45.5; %AC voltage[VAC]
-loc = [85 92 102 112 123 123 134 143 153 162 173 173 186 196 209 219] * 1e-3; %[m] from leading edge
+loc = [85 92 102 112 123 123 134 143 153 162 173 173 186 196 209 219].' * 1e-3; %[m] from leading edge
 %measured dims
 fanFreq = 9; %[Hz]
 T_amb = 273.15 + 20.8; %[K]
 T_inf = T_amb;
-P_amb = 87.939447; %[kPa]
-P_dyn = 0.0584774; %[kPa]
-[rho,mu,k,Cp] = AirProperties(T_amb, P_amb*1e3);
-V_fs = sqrt(2*P_dyn/rho); %free stream velocity[m/s]
+P_amb = 87.939447e3; %[Pa]
+P_dyn = 0.0584774e3; %[Pa]
+[rho,mu,k,Cp] = AirProperties(T_amb, P_amb);
+V_fs = sqrt((2*P_dyn)/rho); %free stream velocity[m/s]
+platePower = V_AC^2/(R); %[W]
+
+
 
 %% 1, calcs from measured data
 %a, heat flux from top surf
@@ -41,10 +45,10 @@ localHeatTransCoeff = netHeatFlux_top./(T_s-T_inf); %[W/(m^2*K)]
 avgHeatTransCoeff = 1/(loc(length(loc))-loc(1)) * trapz(loc, localHeatTransCoeff);
 %c, nusselt num
 T_f = (T_s + T_inf)/2; %film temp[K]
-[rho_f, mu_f, k_f, Cp_f] = AirProperties(T_f, P_amb*1e3);
+[rho_f, mu_f, k_f, Cp_f] = AirProperties(T_f, P_amb);
 localNusselt = localHeatTransCoeff.*loc./k_f; 
 avgT_f = mean(T_f);
-[rho_bar, mu_bar, k_bar, Cp_bar] = AirProperties(avgT_f, P_amb*1e3);
+[rho_bar, mu_bar, k_bar, Cp_bar] = AirProperties(avgT_f, P_amb);
 avgNusselt = avgHeatTransCoeff*L./k_bar;
 
 %% 2, theoretical predictions
@@ -55,7 +59,7 @@ Re_cr = 5e5;
 
 % theoreticalLocalNusselt_laminar = (0.453*Re_x.^(0.5)*Pr^(1/3))/(1 - (zeta./loc).^(3/4)).^(1/3);
 % theoreticalLocalNusselt_turbulent = (0.031*Re_x.^(4/5)*Pr^(1/3))/(1 - (zeta./loc).^(9/10)).^(1/9);
-theoreticalLocalNusselt = zeros(length(Re_x),1);
+theoreticalLocalNusselt = zeros(size(Re_x));
 
 for i = 1:length(Re_x)
     if Re_x(i) <= Re_cr
@@ -94,8 +98,63 @@ heatTransRad = L_h*w*heatFluxRad_L;
 
 %% Plots
 x_prime = (loc-zeta)./L_h; %nondimentional location
+x_prime_top = [x_prime(1:5); x_prime(7:11); x_prime(13:end)];
+x_prime_bot = [x_prime(6);x_prime(12)];
+
+T_s_top = [T_s(1:5); T_s(7:11); T_s(13:end)];
+T_s_bot = [T_s(6);T_s(12)];
+
 
 %% 1a
+theoLocalSurfTemp_CR = T_inf + (netHeatFlux_top-heatFluxRad)./theoreticalLocalHeatTransCoeff;
+
 close all;
 figure();
-plot(x_prime, T_s);
+hold on;
+plot(x_prime_top, T_s_top, 'bo', 'DisplayName', 'Experiment Top Surface');
+plot(x_prime_bot, T_s_bot, 'rs', 'DisplayName', 'Experiment Bot Surface');
+plot(x_prime, theoLocalSurfTemp, 'k-', 'DisplayName', 'Theoretical Surface Due to Convection');
+plot(x_prime, theoLocalSurfTemp_CR, 'k--', 'DisplayName', 'Theoretical Surface Due to Conv and Rad');
+title('Justin Francis, Figure 1a');
+xlabel('Nondimensional Length, x"[rad]');
+ylabel('Surface Temperature, T_s[K]');
+grid();
+legend('Location', 'southeast');
+saveas(gcf, 'Fig1a.png');
+%% 1b
+figure();
+hold on;
+plot(x_prime, localHeatTransCoeff, 'ro', 'DisplayName', 'Experimental');
+plot(x_prime, theoreticalLocalHeatTransCoeff, 'k-', 'DisplayName', 'Theoretical');
+title('Justin Francis, Figure 1b');
+xlabel('Nondimensional Length, x"[rad]');
+ylabel('Local Heat Trans Coeff, h_x[W/m^2*K]');
+grid();
+legend('Location', 'northeast');
+saveas(gcf, 'Fig1b.png');
+%% 1c
+figure();
+hold on;
+plot(x_prime, localNusselt, 'ro', 'DisplayName', 'Experimental');
+plot(x_prime, theoreticalLocalNusselt, 'k-', 'DisplayName', 'Theoretical');
+title('Justin Francis, Figure 1c');
+xlabel('Nondimensional Length, x"[rad]');
+ylabel('Local Nusselt Number, Nu_x[rad]');
+grid();
+legend('Location', 'northeast');
+saveas(gcf, 'Fig1c.png');
+%% short ans
+%2a
+errNux = (localNusselt - theoreticalLocalNusselt)./theoreticalLocalNusselt .* 100;
+errhx = (localHeatTransCoeff - theoreticalLocalHeatTransCoeff)./theoreticalLocalHeatTransCoeff .* 100;
+errTs = (T_s - theoLocalSurfTemp)./theoLocalSurfTemp .* 100;
+rangeNux = range(errNux);
+rangehx = range(errhx);
+rangeTs = range(errTs);
+
+%2b
+errNu =(avgNusselt - theoAvgNusselt)./theoAvgNusselt .* 100;
+errh = (avgHeatTransCoeff - theoAvgHeatTransCoeff)./theoAvgHeatTransCoeff .* 100;
+
+%2c
+perLostToRad = heatFluxRad_L/netHeatFlux_top * 100;
